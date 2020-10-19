ea9ce66afffe76c773d7a606b2e217f735e4b846
alireza <alireza.tehran2020@gmail.com>
2020-10-19 15:39:17 +0330

    general design of nav bar is finished!

---
+++ nav.js
@@ -0,0 +1,23 @@
+const navbar = () => {
+    const nav_mob = document.querySelector('.nav-mob');
+    const nav_container_mob = document.querySelector('.nav-container-mob');
+    const body = document.querySelector("body");
+    const navLi = document.querySelectorAll('.nav-container-mob li');
+    console.log(navLi);
+
+    nav_mob.addEventListener('click', () => {
+        nav_container_mob.classList.toggle('nav-container-mob-active');
+        body.classList.add('fix-body');
+
+        navLi.forEach((link, index) => {
+            link.style.animation = `nav-animation 1s ease forwards ${index/6}s`;
+            console.log(link.style.animation);
+        });
+
+    });
+
+
+}
+
+
+navbar();
